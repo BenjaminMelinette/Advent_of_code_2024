76f511c5466a4db1f80bbb655c75b1712138fd7c
BenjaminMelinette <benjamin.melinette@epitech.eu>
2024-12-11 21:54:01 +0100

    [ADD] Day_8

--- Day_8/Day_8.py
+++ Day_8/Day_8.py
@@ -5,8 +5,14 @@ def main():
     part_2()
 
 def part_2():
+    return
 
 def part_1():
+    tab = []
+    with open("Day_8_input.txt", 'r', encoding="utf-8") as file:
+        for line in file:
+            tab.append(line.strip())
+    print("J'ai pas compris le Sujet")
 
 
 if __name__ == "__main__":
