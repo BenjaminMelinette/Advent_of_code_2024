bc460ef9a7129cb3e7859f4d037414f3ba2ac0ac
BenjaminMelinette <benjamin.melinette@epitech.eu>
2024-12-11 21:53:22 +0100

    [ADD] Disk_Representation

--- Day_9/Day_9.py
+++ Day_9/Day_9.py
@@ -5,9 +5,19 @@ def main():
     part_2()
 
 def part_2():
+    return
 
 def part_1():
-
+    disk_representation = ""
+    with open("Day_9_input.txt", 'r', encoding="utf-8") as file:
+        for line in file:
+            disk = line.strip("")
+        for i in range(len(disk)):
+            if (i % 2 == 0):
+                disk_representation += f'{int(i / 2)}' * int(disk[i])
+            else:
+                disk_representation += '.' * int(disk[i])
+    
 
 if __name__ == "__main__":
     main()
