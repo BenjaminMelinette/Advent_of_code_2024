1f85ffe4a9dd8efb0bc943b9416095cf11f52ed5
BenjaminMelinette <benjamin.melinette@epitech.eu>
2025-01-05 14:41:46 +0100

    [ADD] Day 25 part 1

--- Day_25/Day_25.py
+++ Day_25/Day_25.py
@@ -5,8 +5,34 @@ def main():
     part_2()
 
 def part_2():
+    return
 
 def part_1():
+    locks = []
+    keys = []
+    total = 0
+    with open("Day_25_input.txt", 'r', encoding="utf-8") as file:
+        shematics = file.read().split('\n\n')
+        for shematic in shematics:
+            ligns = shematic.strip().split("\n")
+            values = [-1, -1, -1, -1, -1]
+            for lign in ligns:
+                for i in range(5):
+                    if lign[i] == '#':
+                        values[i] += 1
+            if (ligns[0] == "#####"):
+                locks.append(values)
+            else:
+                keys.append(values)
+    for key in keys:
+        for lock in locks:
+            valid = True
+            for i in range(5):
+                if (lock[i] + key[i] > 5):
+                    valid = False
+            if (valid == True):
+                total += 1
+    print(total)
 
 
 if __name__ == "__main__":
